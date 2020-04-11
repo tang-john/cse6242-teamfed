18bf79ebdb59f514fc58cede02e54b12a6cdc0cd
Chavez, Jason Isaac <jchavez8@gatech.edu>
2020-04-11 11:11:11 -0400

    Updated R code that includes Time Series evaluation
    Updates include Time Series evaluation to determine Household Debt autocorrelation, along with other added visualizations.

--- rcode/VariableEvaluation.R
+++ rcode/VariableEvaluation.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(GGally)
 library(rpart)
 library(caret)
+library(glmnet)
 
 team_fed <- read_csv("C:/Users/Jason/Downloads/masterdata.csv")
 
@@ -20,8 +21,7 @@ bad_cols <- c("yearQtr", "autoLoan", "year", "Qtr", "effFundsRate",
               "studentLoan")
 
 test <- team_fed[, -which(names(team_fed) %in% bad_cols)]
-
-test["MortgageDebt_Millions"] <- as.double(test["MortgageDebt_Millions"])
+# test <- team_fed[ , -which(names(team_fed) %in% c("yearQtr", "year", "Qtr"))]
 
 ggpairs(test)
 
@@ -42,3 +42,56 @@ importance <- varImp(model, scale = F)
 plot(importance)
 
 cor(master_NoNA$studentLoan, master_NoNA$householdDebtToGdp)
+
+cor(master_NoNA$gdp, master_NoNA$medianHomePrice)
+
+##################### Using LASSO Regression #################################
+
+x <- as.matrix(master_NoNA[, -1])
+y <- as.double(as.matrix(master_NoNA[, 1]))
+
+x.scaled <- scale(x)
+y.scaled <- scale(y)
+
+fed_lasso <- cv.glmnet(x.scaled, y.scaled, standardize = F, alpha = 1,
+                         family = "gaussian")
+
+summary(fed_lasso)
+fed_lasso$lambda.min
+
+coef(fed_lasso$glmnet.fit, s = fed_lasso$lambda.min)
+
+plot(fed_lasso, xvar = 'lambda')
+
+############################ Time Series Evaluation ############################
+
+library(tidyverse)
+library(outliers)
+library(forecast)
+library(utils)
+
+debt <- read_csv("C:/Users/Jason/Downloads/householdDebtTimeSeries.csv", col_names = F)
+
+glimpse(debt)
+
+debt_vec <- as.vector(debt)
+
+debt_ts <- ts(debt_vec, start = 1999, frequency = 4)
+
+plot(debt_ts)
+
+smoothed <- HoltWinters(debt_ts, alpha = NULL, seasonal = "multiplicative")
+
+smoothed$alpha
+smoothed$beta
+smoothed$gamma
+
+plot(smoothed)
+plot(smoothed$fitted)
+plot(decompose(debt_ts))
+
+acf(debt_ts)
+
+forecast_1year <- forecast(smoothed, h = 4, findfrequency = TRUE)
+
+plot(forecast_1year)
